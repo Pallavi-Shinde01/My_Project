01d0aad073c0891c269e3d09b6f95cf38b33c743
Pallavi Shinde <pallavishinde22045@gmail.com>
2025-07-04 13:40:53 +0530

    first commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,7 +27,7 @@ const HomePage = () => {
             <p><b>Vivekanand College</b>is a premier educational institution dedicated to fostering academic excelence, unnovation, and holistic development. Established in 1980, we have proudly served generations of students, empowering them to achieve their full potential.
             <br/><br/>At Vivekanand College, we believe in a vibrant learning environment that extends beyond textbooks. Our state-of-the-art facilities, experienced faculty, and diverse student community create a unique ecosystem where curiosity thrives and future leaders are shaped.</p>
             <h2>Why Choose Vivekanand College?</h2>
-            <hr/>
+            
 
             <ul>
                 <li><b>Legacy of Excellence:</b> Decades of commitment to quality education.</li>
